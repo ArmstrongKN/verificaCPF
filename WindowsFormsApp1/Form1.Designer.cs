f7625247bf3902c804c76659e0962d2e53a7cf5f
ArmstrongKN <kioshinambu@hotmail.com>
2023-10-10 21:08:22 -0300

    valida CPF

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.maskedCPF = new System.Windows.Forms.MaskedTextBox();
+            this.btnVerifica = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.Location = new System.Drawing.Point(139, 50);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(69, 13);
+            this.lblCPF.TabIndex = 0;
+            this.lblCPF.Text = "Dig. seu CPF";
+            // 
+            // maskedCPF
+            // 
+            this.maskedCPF.Location = new System.Drawing.Point(142, 92);
+            this.maskedCPF.Mask = "999999999-99";
+            this.maskedCPF.Name = "maskedCPF";
+            this.maskedCPF.Size = new System.Drawing.Size(100, 20);
+            this.maskedCPF.TabIndex = 1;
+            this.maskedCPF.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // btnVerifica
+            // 
+            this.btnVerifica.Location = new System.Drawing.Point(142, 163);
+            this.btnVerifica.Name = "btnVerifica";
+            this.btnVerifica.Size = new System.Drawing.Size(75, 23);
+            this.btnVerifica.TabIndex = 2;
+            this.btnVerifica.Text = "verifica";
+            this.btnVerifica.UseVisualStyleBackColor = true;
+            this.btnVerifica.Click += new System.EventHandler(this.btnVerifica_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVerifica);
+            this.Controls.Add(this.maskedCPF);
+            this.Controls.Add(this.lblCPF);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.MaskedTextBox maskedCPF;
+        private System.Windows.Forms.Button btnVerifica;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerifica_Click(object sender, EventArgs e)
+        {
+            string cpf = maskedCPF.Text;
+            Class_Validacao validacao = new Class_Validacao();
+            bool verifica = true;
+            verifica = validacao.validaCPF(cpf);
+            if(verifica == true)
+            {
+                MessageBox.Show("CPF válido!");
+            }
+            else
+            {
+                MessageBox.Show("CPF inválido!");
+            }
+
+        }
+    }
+}
